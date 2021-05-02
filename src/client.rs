5d5a8b4e545d5e5cccb6ce2f3b08ad3d2e008fa9
dougfort <doug.fort@gmail.com>
2021-05-02 13:32:17 -0400

    basic client

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,21 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    unimplemented!()
+use twodipd::ipd_client::IpdClient;
+use twodipd::JoinRequest;
+
+pub mod twodipd {
+    tonic::include_proto!("twodipd");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = IpdClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(JoinRequest {
+        player_name: "Tonic".into(),
+    });
+
+    let response = client.join(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
 }
